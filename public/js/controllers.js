3034de26eff10caecee27b323c9f6bcd9c75b359
Evan Bechtol <evanbechtol@gmail.com>
2016-05-29 14:16:40 -0500

    testing

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -16,6 +16,6 @@ dotaApp.controller('matchController', ['$scope', '$routeParams', 'matchDetailsSe
     $scope.matchResult = ' ';
     $scope.matchResult = matchDetailsService.getMatch($scope.matchId);
    // $scope.matchResult = $scope.matchResult[1];
-    console.log(JSON.stringify(_.pick($scope.matchResult, 'players')));
-    console.log($scope.matchResult.players);
+    console.log(JSON.stringify(_.pairs($scope.matchResult)));
+    console.log($scope.matchResult);
 }]);
