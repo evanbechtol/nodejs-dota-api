6eddb85b69eb7ccd546cdf549edf766ed45db904
Evan Bechtol <evanbechtol@gmail.com>
2016-05-30 12:08:57 -0500

    testing

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -14,8 +14,8 @@ dotaApp.controller('matchController', ['$scope', '$routeParams', 'matchDetailsSe
     $scope.matchId = matchService.matchId;
     matchDetailsService.getMatch($scope.matchId).$promise.then(function (match) {
         $scope.match = _.pick(match, 'result');
-        $scope.match = $scope.match.result;
-        $scope.players = $scope.match.players;
+        //$scope.match = $scope.match.result;
+        $scope.players = $scope.match.result.players;
         console.log(JSON.stringify($scope.players));
     }, function (err) {
         console.error(err);
@@ -27,8 +27,18 @@ dotaApp.controller('matchController', ['$scope', '$routeParams', 'matchDetailsSe
     $scope.convert64to32 = function(id) {
         return dotaApi.convert64to32(id);
     };
-    $scope.getPlayerName = function(id) {
-        
+    $scope.getPlayerSummary = function(id) {
+        dotaApi.getPlayerSummaries($scope.convert32to64(id)).$promise.then(function (summary) {
+            
+            if (_.has(summary.response, 'players')) {
+                var player = [summary.personaname, summary.avatar];
+                return player;
+            } else {
+                return {error: 'Player could not be found'};
+            }
+        }, function (err) {
+            return err;
+        });
     }
     // $scope.matchResult = $scope.matchResult[1];
 
