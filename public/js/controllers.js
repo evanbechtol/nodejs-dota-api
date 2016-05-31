cbb570632e25043fdaed72fddb13d991dff48d3f
Evan Bechtol <evanbechtol@gmail.com>
2016-05-30 21:59:26 -0500

    testing

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -31,7 +31,7 @@ dotaApp.controller('matchController', ['$scope', '$routeParams', 'matchDetailsSe
     };
     
     var getPlayerSummary  = function(id) {
-        dotaApi.getPlayerSummaries($scope.convert32to64(id)).$promise.then(function (summary) {
+        dotaApp.getPlayerSummaries($scope.convert32to64(id)).$promise.then(function (summary) {
             
             if (_.has(summary.response, 'players')) {
                 var player = {personaname: summary.personaname, avatar: summary.avatar};
