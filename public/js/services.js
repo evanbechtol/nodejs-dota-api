5dbe7b659a9756a5e5f316cc7d724d849b89fe6d
Evan Bechtol <evanbechtol@gmail.com>
2016-05-30 22:08:26 -0500

    testing

--- public/js/services.js
+++ public/js/services.js
@@ -3,9 +3,15 @@ dotaApp.service('matchService', function() {
 });
 
 dotaApp.service('matchDetailsService', ['$resource', function($resource) {
+    var BASEURL = 'https://nodejs-dota-api.herokuapp.com';
     this.getMatch = function(matchId) {
-        var dotaAPI = $resource("https://nodejs-dota-api.herokuapp.com/matchDetails/?");
+        var dotaAPI = $resource(BASEURL + "/matchDetails/?");
     
         return dotaAPI.get({ match_id: matchId }); 
+    }
+    this.getPlayerSummaries = function(steamId) {
+        var dotaAPI = $resource(BASEURL + "/playerSummaries/?");
+        
+        return dotaAPI.get({steamids: steamId});
     }
 }]);
