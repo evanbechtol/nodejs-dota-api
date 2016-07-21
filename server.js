38441908933b81eaeefaa813db87d19d72ee670e
Evan Bechtol <evanbechtol@gmail.com>
2016-07-21 11:00:04 -0500

    Update index route

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
     res.sendFile('index.html', {root: __dirname});
+    console.log(req.connection.remoteAddress);
 });
 
 app.get('/match', function (req, res) {
